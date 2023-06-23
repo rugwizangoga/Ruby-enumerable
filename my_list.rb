e878e7e6d89a2bd9cb58ae3f56b34ef53976a745
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-06-23 22:07:27 +0200

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -5,8 +5,30 @@ class MyList
     @list = list
   end
 
-  def each
-    @list.each { |element| yield(element) }
+  def eachrequire_relative 'enumerable'
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each { |element| block.call(element) }
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # Output: true
+puts(list.all? { |e| e > 5 }) # Output: false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # Output: true
+puts(list.any? { |e| e == 5 }) # Output: false
+
+# Test #filter
+puts(list.filter(&:even?)) # Output: [2, 4]
   end
 end
 
